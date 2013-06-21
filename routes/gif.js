8adf90d5eda027a7214b48233cce0c8890289ca0
Bryan Stedman <bryan@bryanstedman.com>
2013-06-20 18:53:48 -0700

    Use local version of json file cause I is offline

--- routes/gif.js
+++ routes/gif.js
@@ -5,7 +5,7 @@
 
 exports.list = function(req, res){
   var http = require('http');
-  http.get('http://gifapi.co/gifs.json', function (res2) {
+  http.get('http://localhost:8000/gifs.json', function (res2) {
     res2.on('data', function(d) {
       var parsed = JSON.parse(d);
       console.log(req.params.tag)
